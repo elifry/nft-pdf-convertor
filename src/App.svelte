80d27c1949d8220b8a97f93a38c73e4123fd8fbe
Eli Fry <fryjah07@gmail.com>
2021-09-29 00:11:24 -0400

    Added qr code generator, finished top line of description section

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,18 @@
 <script>
 import { onMount } from "svelte";
+//import { QRCode } from 'qrcode';
+import QrCode from "svelte-qrcode"
 import { apiData, drinkNames } from './store.js';
 
 	let imgSrc = "https://galaxy-eggs-images.s3.amazonaws.com/2k/jpg/3621.jpg";
+	let qrSrc = 'https://opensea.io/assets/0xa08126f5e1ed91a635987071e6ff5eb2aeb67c48/1136'
 	let description = "collection description";
-	let name = "Galaxy Eggs"; // hardcoded because name=GalaxyEggs
-	let eggNumber = 6201;
+	let collectionName = "Galaxy Eggs"; // hardcoded because name=GalaxyEggs
+	let eggNumber = 1136;
 	let series = "series";
 
 onMount(async () => {
-  fetch("https://api.opensea.io/api/v1/assets?token_ids=6201&order_direction=desc&offset=0&limit=1&collection=galaxyeggs9999")
+  fetch(`https://api.opensea.io/api/v1/assets?token_ids=${eggNumber}&order_direction=desc&offset=0&limit=1&collection=galaxyeggs9999`)
   .then(response => response.json())
   .then(data => {
 		console.log(data);
@@ -25,84 +28,97 @@ onMount(async () => {
 </script>
 
 <main>
-	<!-- <div class="displaybox">
-		<h1>Galaxy Eggs PDF Generator</h1>
-		<div class="imgbox">
-		    <img class="center-fit" src={imgSrc} alt="qr code"/>
+	<div class="topSection">
+		Top Section
+	</div>
+	<div class="displaybox">
+		<div><img class="eggImage" src={imgSrc} alt="egg image"/></div>
+		<div class="descriptionSection">
+			<div class='row1'>
+				<div class="collectionSeries">
+					<div class="collectionName">{series}</div>
+					<div class="series">{collectionName}</div>
+				</div>
+				<div class="qrCode">
+					<QrCode value={qrSrc} size="70" />
+				</div>
+				<div class="eggNum">
+					#{eggNumber}
+				</div>
+			</div>
 		</div>
-		<p>{name}</p>
-		<p>{series}</p>
-		<p>{description}</p>
-	</div> -->
-	<div class="wrapper aligned">
+		<!-- <div><img class="qrCode" src={qrSrc} alt="qr code"/></div> -->
+	</div>
+	<!-- <div class="wrapper aligned">
 	    <div class="box item1">
 			<img class="center-fit" src={imgSrc} alt="qr code"/>
 		</div>
-	    <div class="box item2">{name} {series}</div>
+	    <div class="box item2">
+			<div class="collection">{collection}</div>
+			<div class="series">{series}</div>
+		</div>
 	    <div class="box item3">#{eggNumber}</div>
 	    <div class="box item4">(Art)ificial is an art studio that explores the boundaries of technology and art. Our first project is Galaxy Eggs - a generative collection of 9,999 Eggs of the metaverse that live on the Ethereum Blockchain. Our Art Director, Gal Barkan, has been creating futuristic and sci-fi art for the past 20 years - this collection is the culmination of a lifetime of work on one hand, and the beginning of a new chapter in taking part in the creation of the metaverse.</div>
 	    <div class="box item5">table</div>
-	</div>
+	</div> -->
 </main>
 
 <style>
-	* {
-	    margin: 0;
-	    padding: 0;
+	.topSection {
+		height: 5em;
 	}
 	.displaybox {
-	    display: grid;
-	    height: 100%;
+		height: 842px;
+		width: 595px;
 	}
-	.imgbox {
-	    display: grid;
-	    height: 30%;
+	.eggImage {
+		max-width:100%;
+		max-height:100%;
 	}
-	.center-fit {
-	    max-width: 100%;
-	    max-height: 100vh;
-	    margin: auto;
+	.descriptionSection {
+		height: 247px;
+		width: 595px;
 	}
-	html {
-	  box-sizing: border-box;
+	.row1 {
+		display: flex;
 	}
-	*, *:before, *:after {
-	  box-sizing: inherit;
+	.row2 {
+		display: flex;
+	}
+	.collectionSeries {
+		border-left: 4px solid #AFAFAF;
+		flex: 1;
+		margin-top: 25px;
 	}
 
-	body {
-	  margin: 40px;
+	.collectionSeries > .collectionName {
+		padding-left: 12px;
+		font-weight: 400;
+		font-size: 1.2em;
 	}
-
-	.wrapper {
-	  margin: auto;
-	  width: 500px;
-	  height: 100vh;
-	  display: grid;
-	  grid-gap: 4px;
-	  grid-template-columns: 2fr 2fr 1fr 1fr;
-	  grid-template-rows: 2fr 1fr 1fr;
-	  align-content: space-around;
-	  justify-content: space-between;
+	.collectionSeries > .series {
+		padding-left: 12px;
+		font-weight: 600;
+		font-size: 2.5em;
 	}
-	.box {
-	  padding: 20px;
-	  font-size: 150%;
+	.qrCode {
+		flex: 0.4;
+		margin-top: 25px;
 	}
-	.item1 {
-	  grid-column: 1 / 5;
+	.eggNum {
+		margin-top: 10px;
+		flex: 0.4;
+		font-weight: 550;
+		font-size: 5em;
 	}
-	.item2 {
-	  grid-column: 1 / 3;
+	.longDescription {
+		flex: 0.4;
+		font-weight: 550;
+		font-size: 4em;
 	}
-	.item3 {
-	  grid-column: 3 / 5;
-	}
-	.item4 {
-	  grid-column: 1 / 3;
-	  font-size: 0.875em;
-	}
-	.item5 {
-	  grid-column: 3 / 5;
+	.tableArea {
+		flex: 0.4;
+		font-weight: 550;
+		font-size: 4em;
 	}
 </style>
